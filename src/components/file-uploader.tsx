d7680751e146baf201879666ccf1dfc9d9334d9d
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2025-03-06 00:42:44 +0000

    init

---
+++ src/components/file-uploader.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function FileUploader() {
+  return (
+    <div>
+      <input type="file" />
+      <Button>Upload</Button>
+    </div>
+  );
+}
